31e06c9d6b94b64ccd79b918a8c77065fc22d24d
wenderf5 <wenderfabianoalves@gmail.com>
2024-07-19 12:15:53 -0300

    fix: fix controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ArtistModuleModule } from './modules/artist-module/artist-module.module';
 import { PlaylistModuleModule } from './modules/playlist-module/playlist-module.module';
 import { MusicModuleModule } from './modules/music-module/music-module.module';
+import { DefaultController } from './default/default.controller';
 
 @Module({
   imports: [ArtistModuleModule, PlaylistModuleModule, MusicModuleModule],
-  controllers: [],
+  controllers: [DefaultController],
   providers: [],
 })
 export class AppModule {}
---
+++ src/default/default.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class DefaultController {
+
+    @Get('/')
+    DefaultRote(){
+        return "Hello world!"
+    }
+}
