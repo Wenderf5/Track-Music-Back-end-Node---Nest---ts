f01c02a832acaa67d11168112d07c34aacb0e874
wenderf5 <wenderfabianoalves@gmail.com>
2024-07-22 12:48:38 -0300

    fix: fix cors options

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const CorsOptions: CorsOptions =  {
+    origin: ["https://track-music-client.vercel.app", "http://localhost:5173"]
+  }
+  app.enableCors(CorsOptions);
   await app.listen(8080, ()=>{
     console.log("Server running!")
   });
